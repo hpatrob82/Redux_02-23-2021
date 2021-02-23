15ebcd02f70b78e3cfa59fcdaa5a910591e5501d
hilda robles <hprobles82@gmail.com>
2021-02-23 15:33:57 -0500

    edit actions

---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+// import { createStore } from 'redux';
+
+const { createStore } = Redux;
+console.log('App Started!');
+// Store!!
+const defaultState = {
+        balance: 0
+    }
+    // Actions!
+
+
+const actionIncrement = (amount) => {
+    return {
+        type: 'increment',
+        payload: amount
+    }
+}
+const actionDecrement = (amount) => {
+    return {
+        type: 'decrement',
+        payload: amount
+    }
+}
+
+// Reducer
+const account = (state = defaultState, action) => {
+    console.log('action is:', action);
+    switch (action.type) {
+        case "increment":
+            return {
+                balance: state.balance + action.payload,
+            };
+        case "decrement":
+            return {
+                balance: state.balance - action.payload,
+            }
+        default:
+            return state;
+    }
+}
+const store = createStore(account, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+    console.log('Subscribing to state changes...');
+    const state = store.getState();
+    console.log("the state is:", state);
+    const balance = document.querySelector('#balance');
+    balance.innerHTML = state.balance;
+});
+
+
+const incrementButton = document.querySelector('#add');
+const decrementButton = document.querySelector('#subtract');
+const amount = document.querySelector('#amount');
+
+incrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const amountValue = parseInt(amount.value);
+    store.dispatch(actionIncrement(amountValue));
+
+});
+
+decrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const amountValue = parseInt(amount.value);
+    store.dispatch(actionDecrement(amountValue));
+});
+
+
+
+
+
+
+
+
+// WHEN LOADING IN THE BROWSER USE:
+// window.store = store;
+// window.actionIncrement = actionIncrement;
+// window.actionDecrement = actionDecrement;
